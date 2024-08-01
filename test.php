31fa88fb55019ab33c27bfbeba6f23b1434a4e7a
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-01 16:29:00 -0300

    A test file

---
+++ test.php
@@ -0,0 +1,60 @@
+<?php
+require_once("Roman.php");
+$romanNumbers = [
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX',
+    'X',
+    'XI',
+    'XII',
+    'XIII',
+    'XIV',
+    'XV',
+    'XVI',
+    'XVII',
+    'XVIII',
+    'XIX',
+    'XX',
+    'XXI',
+    'XXII',
+    'XXIII',
+    'XXIV',
+    'XXV',
+    'XXVI',
+    'XXVII',
+    'XXVIII',
+    'XXIX',
+    'XXX',
+    'XXXI',
+    'XXXII',
+    'XXXIII',
+    'XXXIV',
+    'XXXV',
+    'XXXVI',
+    'XXXVII',
+    'XXXVIII',
+    'XXXIX',
+    'XL',
+    'XLI',
+    'XLII',
+    'XLIII',
+    'XLIV',
+    'XLV',
+    'XLVI',
+    'XLVII',
+    'XLVIII',
+    'XLIX',
+    'L'
+];
+
+$conversor = new Roman("I");
+foreach ($romanNumbers as $romanNumber) {
+    $conversor->setNumber($romanNumber);
+    echo "$romanNumber: {$conversor->toDecimal()}" . "\n";
+}
