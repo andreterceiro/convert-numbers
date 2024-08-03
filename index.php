e91a57304542a36334385e8799456a036e119852
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-03 19:21:34 -0300

    Updating the main file to convert from decimal to roman

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 require_once('config.php');
 if ($_POST['cmdEnviar'] == 'Converter para romano') {
+    try {
+        require_once('libs/Decimal.php');
+        $romanConversor = new Decimal($_POST['decimal']);
+        $viewManager->set(
+            'result',
+            $romanConversor->toRoman()
+        );
+    } catch (Exception $e) {
+        $viewManager->setError("O número decimal para conversão é inválido");
+    }
 } elseif ($_POST['cmdEnviar'] == 'Converter para decimal') {
     try {
         require_once('libs/Roman.php');
