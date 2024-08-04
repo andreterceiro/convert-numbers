7a0c70f75e90f45ab0b1d9c38ea956e4313fc6c0
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-04 12:01:20 -0300

    Convertendo a mensagem da exception para português

--- index.php
+++ index.php
@@ -9,7 +9,12 @@ if ($_POST['cmdEnviar'] == 'Converter para romano') {
             $romanConversor->toRoman()
         );
     } catch (Exception $e) {
-        $viewManager->setError("O número decimal para conversão é inválido");
+        // The exception message is in English. We wanna a portuguese message. Buecause of this we will not
+        // use $e->getMessage()
+        $viewManager->setError(
+            "O número decimal passado deve ser maior que zero, menor que 4000 e um inteiro" .
+            "(não fracionário nem contendo letras)"
+        );
     }
 } elseif ($_POST['cmdEnviar'] == 'Converter para decimal') {
     try {
@@ -20,7 +25,11 @@ if ($_POST['cmdEnviar'] == 'Converter para romano') {
             $romanConversor->toDecimal()
         );
     } catch (Exception $e) {
-        $viewManager->setError("O número romano para conversão é inválido");
+        // The exception message is in English. We wanna a portuguese message. Buecause of this we will not
+        // use $e->getMessage()
+        $viewManager->setError(
+            'O número romano passado é inválido'
+        );
     }
 }
 $viewManager->render("index");
