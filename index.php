1a1823dff704dbeccc5ed810d618241f4ff3b17f
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-04 13:03:01 -0300

    Several simple adjusts. Please make a diff

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once('config.php');
+use andreterceiro\libs\Decimal;
+use andreterceiro\libs\Roman;
+
 if ($_POST['cmdEnviar'] == 'Converter para romano') {
     try {
         require_once('libs/Decimal.php');
@@ -8,12 +11,12 @@ if ($_POST['cmdEnviar'] == 'Converter para romano') {
             'result',
             $romanConversor->toRoman()
         );
-    } catch (Exception $e) {
+    } catch (\Exception $e) {
         // The exception message is in English. We wanna a portuguese message. Buecause of this we will not
         // use $e->getMessage()
         $viewManager->setError(
-            "O número decimal passado deve ser maior que zero, menor que 4000 e um inteiro" .
-            "(não fracionário nem contendo letras)"
+            'O número decimal passado deve ser maior que zero, menor que 4000 e um inteiro' .
+            ' (não fracionário nem contendo letras)'
         );
     }
 } elseif ($_POST['cmdEnviar'] == 'Converter para decimal') {
@@ -24,8 +27,8 @@ if ($_POST['cmdEnviar'] == 'Converter para romano') {
             'result',
             $romanConversor->toDecimal()
         );
-    } catch (Exception $e) {
-        // The exception message is in English. We wanna a portuguese message. Buecause of this we will not
+    } catch (\Exception $e) {
+        // The exception message is in English. We wanna a portuguese message. Because of this we will not
         // use $e->getMessage()
         $viewManager->setError(
             'O número romano passado é inválido'
